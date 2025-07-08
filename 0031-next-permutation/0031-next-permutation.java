fb505c23a97837a6dae3b483c9e92665079e8722
blisss <138624690+blissbug@users.noreply.github.com>
2025-07-09 01:19:28 +0530

    0031-NEXT-PERMUTATION Stats: Time: 0 ms (100%), Space: 43.1 MB (74.21%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -4,40 +4,40 @@ class Solution {
             return;
         }
         int k = -1;
-        int l = -1;
-        for(int i=0;i<nums.length;i++){
-            if(i!=nums.length-1 && nums[i]<nums[i+1]){
+        //start from end to optimize
+        for(int i=nums.length-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
                 k=i;
+                break;
             }
-            if(k!=-1 && k!=i){
-                if(nums[k]<nums[i]){
-                    l=i;
-                }
-            }
+            
         }
-        if(l==-1 && k==-1){
-            Arrays.sort(nums);
-        }
-        else if(l==-1){
-            int temp = nums[k];
-            nums[k] = nums[k+1];
-            nums[k+1] = temp;
+        if(k==-1){
+            reverse(nums,0,nums.length);
         }
         else{
-            int temp = nums[k];
-            nums[k] = nums[l];
-            nums[l] = temp;
-            int sub=1;
-            int mid = nums.length-k;
-            mid = mid/2;
-            for(int i=k+1;i<k+1+mid;i++){
-                temp = nums[i]; 
+            for(int i=nums.length-1;i>=k+1;i--){
+                if(nums[i]>nums[k]){
+                    int temp = nums[k];
+                    nums[k] = nums[i];
+                    nums[i] = temp;
+                    break;
+                }
+            }
+            reverse(nums,k+1,nums.length);
+        }
+
+    }
+    public static void reverse(int[] nums,int st,int end){
+        int sub=1;
+        int mid = end-st;
+        mid = mid/2;
+        for(int i=st;i<st+mid;i++){
+                int temp = nums[i]; 
                 nums[i] = nums[nums.length-sub];
                 nums[nums.length-sub] = temp;
                 sub++;
                 
             }
-        }
-
     }
 }
