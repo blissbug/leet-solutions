b1e878f070943cba558471ef2ec5e8814dde2f92
blisss <138624690+blissbug@users.noreply.github.com>
2025-11-09 18:37:24 +0530

    0001-TWO-SUM Stats: Time: 0 ms (100%), Space: 19.2 MB (10.35%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         map = {}
-        lst = []
         for i,v in enumerate(nums):
             if(map.get(v)!=None):
-                lst.append(map[v])
-                lst.append(i)
-                return lst
+                return [map[v],i]
             else:
                 map[target-v]=i
 
