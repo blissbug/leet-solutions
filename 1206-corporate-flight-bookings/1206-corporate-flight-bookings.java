4ef8640bbe0d13e1c2d4a049fb08d6e93954ff7c
blisss <138624690+blissbug@users.noreply.github.com>
2025-11-19 01:58:27 +0530

    1206-CORPORATE-FLIGHT-BOOKINGS Stats: Time: 3 ms (97.11%), Space: 84.5 MB (23.47%) - LeetHub

--- 1206-corporate-flight-bookings/1206-corporate-flight-bookings.java
+++ 1206-corporate-flight-bookings/1206-corporate-flight-bookings.java
@@ -1,11 +1,22 @@
 class Solution {
     public int[] corpFlightBookings(int[][] bookings, int n) {
-        int[] res = new int[n];
+        int[] diff_arr = new int[n];
         for(int i=0;i<bookings.length;i++){
-            for(int j = bookings[i][0]-1;j<bookings[i][1];j++){
-                res[j] += bookings[i][2];
+            int l = bookings[i][0];
+            int r = bookings[i][1];
+            int diff = bookings[i][2];
+            diff_arr[l-1]+=diff;
+            if(r<n){
+                diff_arr[r]-=diff;
             }
         }
+        int[] res = new int[n];
+        int sum = 0;
+        for(int i=0;i<res.length;i++){
+            sum+=diff_arr[i];
+            res[i]=sum;
+        }
         return res;
+        
     }
 }
