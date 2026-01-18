d6e83d2ca5e82a47b5813f4d971b982ac5aee0f0
blisss <138624690+blissbug@users.noreply.github.com>
2026-01-19 00:34:36 +0530

    0169-MAJORITY-ELEMENT Stats: Time: 10 ms (36.02%), Space: 55.7 MB (52.08%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,18 +1,16 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int count = 0;
-        int res = -1;
-        for(int num:nums){
-            if(count==0){
-                res=num;
-            }
-            if(num==res){
-                count++;
-            }
-            else{
-                count--;
-            }
-        }
-        return res;
+        int ct = 0;
+		Arrays.sort(nums);
+		for(int i=0;i<nums.length;i++) {
+			if(i!=0 && nums[i-1]!=nums[i]) {
+				ct = 0;
+			}
+			ct++;
+            if(ct>=(nums.length+1)/2) {
+				return nums[i];
+			}
+		}
+        return -1;
     }
 }
